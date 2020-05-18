d479448ca9eaa584451bf4eae9b2a92afe47aa37
岸波　雄樹 <you_work_24@icloud.com>
2020-05-18 17:03:18 +0900

    created page transition

--- App.js
+++ App.js
@@ -1,49 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
-import ListItem from './components/ListItem';
-import dummyArticles from './dummies/articles.json';
-import Constants from 'expo-constants';
-import axios from 'axios';
-
-const URL =
-  'https://asia-northeast1-news-app-udemy.cloudfunctions.net/dummy_news';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+import React from 'react';
+import AppNavigattor from './navigation/AppNavigator';
 
 export default function App() {
-  const [articles, setArticles] = useState([]);
-  useEffect(() => {
-    fetchArticles();
-  }, []);
-
-  const fetchArticles = async () => {
-    try {
-      const response = await axios.get(URL);
-      setArticles(response.data.articles);
-      console.log(response);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articles}
-        renderItem={({ item }) => (
-          <ListItem
-            imageURL={item.urlToImage}
-            title={item.title}
-            author={item.author}
-          />
-        )}
-        keyExtractor={(item, index) => item.toString()}
-      />
-    </SafeAreaView>
-  );
+  return <AppNavigattor />;
 }
---
+++ screens/HomeScreen.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import ListItem from '../components/ListItem';
+import Constants from 'expo-constants';
+import axios from 'axios';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
+
+const URL =
+  'https://asia-northeast1-news-app-udemy.cloudfunctions.net/dummy_news';
+
+export default HomeScreen = ({ navigation }) => {
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      setArticles(response.data.articles);
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageURL={item.urlToImage}
+            title={item.title}
+            author={item.author}
+            onPress={() => navigation.navigate('Article', { article: item })}
+          />
+        )}
+        keyExtractor={(item, index) => item.toString()}
+      />
+    </SafeAreaView>
+  );
+};
